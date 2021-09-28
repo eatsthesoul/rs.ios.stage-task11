d2f08fd35ec07db1cb35c1d96ad4a79535f29ca9
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-09-28 16:09:08 +0300

    init commit

---
+++ RSSchool_T11/RSSchool_T11/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  RSSchool_T11
+//
+//  Created by Evgeniy Petlitskiy on 28.09.21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ RSSchool_T11/RSSchool_T11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSSchool_T11
+//
+//  Created by Evgeniy Petlitskiy on 28.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
