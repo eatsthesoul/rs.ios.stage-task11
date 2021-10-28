df020537dd0edbcd42cdb17440ba54898d0cbb29
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-10-28 19:43:17 +0300

    feat: add json parser

---
+++ RSSchool_T11/RSSchool_T11/Parsers/JSONParser.swift
@@ -0,0 +1,31 @@
+//
+//  JSONParser.swift
+//  RSSchool_T11
+//
+//  Created by Evgeniy Petlitskiy on 12.10.21.
+//
+
+import Foundation
+
+class JSONParser<T: Decodable> {
+    
+    private func buildDecoder() -> JSONDecoder {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return decoder
+    }
+    
+    func parseData(_ data: Data, completion: @escaping (T?, Error?) -> ()) {
+        do {
+            let decoder = buildDecoder()
+            let dataObject = try decoder.decode(T.self, from: data)
+            completion(dataObject, nil)
+        } catch let error {
+            completion(nil, error)
+        }
+    }
+    
+    
+}
