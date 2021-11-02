913f8db5d5d64854161922577783cf333b6150d0
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-11-02 13:21:02 +0300

    feat: improve way to represent custom colors, fix: colors for rocket cell

--- RSSchool_T11/RSSchool_T11/Library/Extensions/UIColor.swift
+++ RSSchool_T11/RSSchool_T11/Library/Extensions/UIColor.swift
@@ -9,13 +9,39 @@ import UIKit
 
 extension UIColor {
     
-    static let queenBlue = UIColor(red: 76/255, green: 96/255, blue: 133/255, alpha: 1)
-    static let smokyBlack = UIColor(red: 20/255, green: 20/255, blue: 19/255, alpha: 1)
-    static let slateGray = UIColor(red: 107/255, green: 113/255, blue: 126/255, alpha: 1)
-    static let coral = UIColor(red: 255/255, green: 133/255, blue: 82/255, alpha: 1)
-    static let champagne = UIColor(red: 96/255, green: 91/255, blue: 80/255, alpha: 1)
-    static let white = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
-    static let glaucous = UIColor(red: 111/255, green: 138/255, blue: 183/255, alpha: 1)
-    static let cyanProcess = UIColor(red: 0/255, green: 165/255, blue: 224/255, alpha: 1)
-    static let grayishBlue = UIColor(red: 174/255, green: 174/255, blue: 192/255, alpha: 1)
+    static var queenBlue: UIColor {
+        UIColor(named: "Queen Blue")!
+    }
+    
+    static var smokyBlack: UIColor {
+        UIColor(named: "Smoky Black")!
+    }
+    
+    static var slateGray: UIColor {
+        UIColor(named: "Slate Gray")!
+    }
+    
+    static var coral: UIColor {
+        UIColor(named: "Coral")!
+    }
+    
+    static var champagne: UIColor {
+        UIColor(named: "Champagne")!
+    }
+    
+    static var superWhite: UIColor {
+        UIColor(named: "Super White")!
+    }
+    
+    static var glaucous: UIColor {
+        UIColor(named: "Glaucous")!
+    }
+    
+    static var cyanProcess: UIColor {
+        UIColor(named: "Cyan Process")!
+    }
+    
+    static var shadow: UIColor {
+        UIColor(named: "Shadow")!
+    }
 }
