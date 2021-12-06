1b4ed8a5c9092d63aa92ac98d5204e95c214989d
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-12-06 20:58:16 +0300

    feat: add UIShadowView

---
+++ RSSchool_T11/RSSchool_T11/Library/Views/UIShadowView.swift
@@ -0,0 +1,115 @@
+//
+//  UIShadowView.swift
+//  RSSchool_T11
+//
+//  Created by Evgeniy Petlitskiy on 6.12.21.
+//
+
+import UIKit
+
+class UIShadowView: UIView {
+    
+    private enum LayoutConstants {
+        static let horizontalInset: CGFloat = 10
+        static let verticalInset: CGFloat = 5
+    }
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.textColor = .cyanProcess
+        label.font = UIFont(name: "Roboto-Medium", size: 17)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var foregroundLayer: CALayer = {
+        let layer = CALayer()
+        layer.frame = bounds
+        layer.cornerRadius = bounds.height / 2
+        layer.backgroundColor = UIColor.smokyWhite.cgColor
+        layer.masksToBounds = true
+        return layer
+    }()
+    
+    private lazy var whiteShadowLayer: CALayer = {
+        let layer = CALayer()
+        layer.shadowColor = UIColor.white.cgColor
+        layer.shadowOffset = CGSize(width: -2, height: -2)
+        layer.shadowOpacity = 1
+        layer.shadowRadius = 1.5
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.height / 2).cgPath
+        return layer
+    }()
+    
+    private lazy var greyShadowLayer: CALayer = {
+        let layer = CALayer()
+        layer.shadowColor = UIColor.shadow.cgColor
+        layer.shadowOffset = CGSize(width: 1.5, height: 1.5)
+        layer.shadowOpacity = 1
+        layer.shadowRadius = 1.5
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.height / 2).cgPath
+        return layer
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setupUI() {
+        backgroundColor = .smokyWhite
+        setupLabel()
+        addShadow()
+    }
+    
+    private func setupLabel() {
+        addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: LayoutConstants.horizontalInset),
+            label.topAnchor.constraint(equalTo: topAnchor, constant: LayoutConstants.verticalInset),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -LayoutConstants.horizontalInset),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -LayoutConstants.verticalInset),
+        ])
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateShadow()
+    }
+
+}
+
+//MARK: - API
+extension UIShadowView {
+    func setText(_ text: String) {
+        self.label.text = text
+    }
+}
+
+// MARK: - Shadow methods
+private extension UIShadowView {
+    
+    func addShadow() {
+        layer.insertSublayer(foregroundLayer, at: 0)
+        layer.insertSublayer(whiteShadowLayer, at: 0)
+        layer.insertSublayer(greyShadowLayer, at: 0)
+        layer.cornerRadius = bounds.height / 2
+    }
+    
+    func updateShadow() {
+        foregroundLayer.frame = bounds
+        foregroundLayer.cornerRadius = bounds.height / 2
+        whiteShadowLayer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.height / 2).cgPath
+        greyShadowLayer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.height / 2).cgPath
+        layer.cornerRadius = bounds.height / 2
+    }
+}
